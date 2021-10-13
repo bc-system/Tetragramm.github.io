2b2854f4fe247cda5e9157d27cef4a8a70f49d61
bc-system <92414848+bc-system@users.noreply.github.com>
2021-10-12 19:35:29 -0700

    Update temp.js

--- temp.js
+++ temp.js
@@ -1,5 +1,5 @@
 const init = () => {
-    console.log("/PlaneBuilder/index.html"+location.search);
+    console.log("/Tetragramm.github.io/PlaneBuilder/index.html"+location.search);
     window.history.replaceState(null, null, "/Tetragramm.github.io/PlaneBuilder/index.html"+location.search);
     window.history.go();
 };
